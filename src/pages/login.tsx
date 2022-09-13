a06ccd4bbaf284ae78912357e195c5386b05000d
Victor Hernandez <victor-2027@hotmail.com>
2022-09-13 16:35:50 -0500

    feat:

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NextPage } from 'next';
-import LoginPage from '@components/Login/LoginPage';
 import { LoginProvider } from '@contexts/login.context';
+import LoginPage from '@components/Login/LoginPage';
 
 const Login: NextPage = () => {
   return (
